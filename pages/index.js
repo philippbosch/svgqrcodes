fca8f08c18c511d42cc599c487d1396208a4e31a
Philipp Bosch <hello@pb.io>
2020-11-30 11:57:28 +0100

    fix: address lighthouse issues

--- pages/index.js
+++ pages/index.js
@@ -26,18 +26,20 @@ export default function Home({ url: initialUrl, svg: initialSvg }) {
     <div className="bg-lime-50 min-h-screen flex items-center justify-around">
       <Head>
         <title>QR Code SVG Generator</title>
+        <meta name="description" content="No-frills SVG QR code generator" />
       </Head>
 
       <main className="w-full h-screen sm:h-auto sm:max-w-lg p-4 sm:p-6 lg:p-8 xl:p-10 bg-white sm:rounded-xl sm:shadow-lg text-center">
-        <h1 className="text-2xl sm:text-4xl text-lime-600 font-semibold">
-          QR Code SVG Generator
+        <h1 className="text-2xl sm:text-4xl text-lime-600 font-semibold tracking-tight">
+          <a href="/">QR Code SVG Generator</a>
         </h1>
 
         <form className="mt-8" onSubmit={handleSubmit}>
           <div className="my-2">
-            <input type="url" name="url" value={url} onChange={e => setUrl(e.target.value)} required id="url" className="shadow-sm focus:ring-lime-500 focus:border-lime-500 block w-full sm:text-sm lg:text-xl text-gray-700 border-gray-300 rounded-md" placeholder="https://www.yourwebsite.com/" />
+            <label htmlFor="url" className="sr-only">URL</label>
+            <input type="url" name="url" id="url" value={url} onChange={e => setUrl(e.target.value)} required id="url" className="shadow-sm focus:ring-lime-500 focus:border-lime-500 block w-full sm:text-sm lg:text-xl text-gray-700 border-gray-300 rounded-md" placeholder="https://www.yourwebsite.com/" />
           </div>
-          <button type="submit" className="block w-full items-center px-6 py-3 border border-transparent text-base sm:text-lg font-medium rounded-md shadow text-white bg-lime-600 hover:bg-lime-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-lime-500">
+          <button type="submit" className="block w-full items-center px-6 py-3 border border-transparent text-base sm:text-lg font-medium rounded-md shadow text-white bg-lime-700 hover:bg-lime-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-lime-500">
             Generate QR Code
           </button>
         </form>
